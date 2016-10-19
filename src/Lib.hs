4563fde7edac0577c7ce5b5819ea61899112d66d
Jos van Bakel <jos@codeaddict.org>
2016-10-19 20:50:44 +0200

    UPD: first attempt at solve

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,14 +4,12 @@ module Lib
     ( solve
     ) where
 
-import           Control.Arrow (first)
 import           Data.List
 import           Data.Maybe
-import           Prelude       hiding (Word)
+import           Prelude    hiding (Word)
 
 type Word = String
 type WordList = [Word]
-type Shape = (Int, Int)
 
 data Letter
   = Horz Char
@@ -45,9 +43,9 @@ isEmpty _     = False
 
 
 isPartial :: Pixel -> Bool
-isPartial (Letter (Both _)) = False
-isPartial Black             = False
-isPartial _                 = True
+isPartial (Letter (Horz _)) = True
+isPartial (Letter (Vert _)) = True
+isPartial _                 = False
 
 
 matchLetter :: Pixel -> Char -> Bool
@@ -61,14 +59,15 @@ extendLetter (Vert ch) = Both ch
 extendLetter l         = l
 
 
-completed :: Puzzle -> Bool
-completed = all $ all $ not . isEmpty
+noPartials :: Puzzle -> Bool
+noPartials = all $ all $ not . isPartial
 
 
 letter :: Letter -> Char
 letter (Vert ch) = ch
 letter (Horz ch) = ch
 letter (Both ch) = ch
+
 
 flipLetter :: Letter -> Letter
 flipLetter (Vert ch) = Horz ch
@@ -80,8 +79,8 @@ emptyRow :: Int -> Row
 emptyRow w = replicate w Empty
 
 
-emptyPuzzle :: Shape -> Puzzle
-emptyPuzzle (w, h) = replicate h $ emptyRow w
+emptyPuzzle :: Int -> Int -> Puzzle
+emptyPuzzle w h = replicate h $ emptyRow w
 
 
 flipPuzzle :: Puzzle -> Puzzle
@@ -90,51 +89,42 @@ flipPuzzle = fmap (fmap flipPixel) <$> transpose where
   flipPixel p          = p
 
 
-filterWords :: Shape -> WordList -> WordList
-filterWords (w, h) = filter (\word -> length word <= max w h)
+solve :: WordList -> Puzzle -> [Puzzle]
+solve [] puzzle = [puzzle]
+solve (word:rest) puzzle =
+  concatMap (solve rest) $
+    (flipPuzzle <$> fitWordIntoPuzzle word puzzle)
+  ++ solve rest puzzle -- Also try it without this word
 
 
-solve :: WordList -> Shape -> Puzzle
-solve words shape = undefined
-  -- try every word on position 1
-  --   if it fits.
-  --   make a black after it
-  --   recurse with:
-  --     words = words with word removed
-  --     solution with word filled in
-  -- if no more word fits: then stop
+-- All possible permutations of fitting a word into a puzzle
+fitWordIntoPuzzle :: Word -> Puzzle -> [Puzzle]
+fitWordIntoPuzzle word = concatMapRows (fitWordIntoRow word)
 
 
--- given the current puzzle, try to fill in a word from the list
-iter :: WordList -> Puzzle -> Puzzle
-iter [] puzzle               = puzzle
-iter _ []                    = []
-iter (word:words) (row:rows) = undefined
-  -- test all possible offsets: [1..(length row - length word)]
-  -- for each offset:
-  --   if word fits:
-  --     if there is a next cell after word:
-  --       it should be black or Nothing
-  --       if nothing then set it to Black
-  --     recurse on iter
+concatMapRows :: (Row -> [Row]) -> Puzzle -> [Puzzle]
+concatMapRows rowFn puzzle =
+  concatMap mapRow $ init $ subLists puzzle where
+    mapRow (h, row:t) = map (assemblePuzzle h t) $ rowFn row
+    assemblePuzzle h t row = h ++ [row] ++ t
 
-  -- offsets = [1..(length row - length word)]
-  -- fitWord word row
-  -- for every word in words
-    -- for y in 1..h
-      -- for x in 1..w
-        -- check if word fits horizontal
-          -- if it does: fill it in and recurse
-        -- -- check if word fits vertical (maybe via flipping)
 
 -- All possible permutations of fitting a word in a row
-fitWordPermutations :: Word -> Row -> [Row]
-fitWordPermutations word row = mapMaybe tryFit rowOffsets where
+fitWordIntoRow :: Word -> Row -> [Row]
+fitWordIntoRow word row = mapMaybe tryFit rowOffsets where
   minLength r = length r >= length word
   -- Split row into all possible offets. Tail must have min length of word
   rowOffsets = filter (minLength . snd) $ subLists row
+  -- Fits are only viable if either the last of head is [], Empty or Black
+  -- When Empty it is replaced by Black
+  tryFit :: (Row, Row) -> Maybe Row
+  tryFit ([], t) = fitWord word t
+  tryFit (h, t) = case last h of
+    Empty -> tryFitAppend (init h ++ [Black]) t
+    Black -> tryFitAppend h t
+    _     -> Nothing
   -- If the tail fits, then append the head
-  tryFit (h, t) = (++) h <$> fitWord word t
+  tryFitAppend h t = (++) h <$> fitWord word t
 
 
 -- Try to fit a word in a row. Fills the word into the row if it fits.
@@ -167,5 +157,5 @@ fitWord (ch:word) (pixel:row) =
 subLists :: [a] -> [([a], [a])]
 subLists = subListsAcc [] where
   subListsAcc :: [a] -> [a] -> [([a], [a])]
-  subListsAcc acc []     = []
-  subListsAcc acc (x:xs) = (acc, xs) : subListsAcc (acc ++ [x]) xs
+  subListsAcc acc []        = [(acc, [])]
+  subListsAcc acc xs@(y:ys) = (acc, xs) : subListsAcc (acc ++ [y]) ys
